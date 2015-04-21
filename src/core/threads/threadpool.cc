e43c208845915b97d94b7fa96a72887325bc059c
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2015-04-21 12:00:40 +0200

    *thread* - Added function for templated lambda thread functions. - Thread functions now runs within an internal lambda, where the outer handles the running state of the thread. - Thread pools capture the function inputs and outputs. I would actually rather be able to send a function using std::bind, but there is no way to store a function without knowing its signature. - Thread pools runs through free threads and assigns them a function, active threads are checked for completion and removed. Needs testing. - Thread runs delete on std::thread, because we might actually want to run the destructor and not just dealloc...

--- src/core/threads/threadpool.cc
+++ src/core/threads/threadpool.cc
@@ -3,6 +3,7 @@
     @class JARVIS::Core::ThreadPool
  	(C) 2015 See the LICENSE file.
 */
+#include "config.h"
 #include "threadpool.h"
 namespace JARVIS {
 namespace Core
@@ -15,6 +16,7 @@ ThreadPool::ThreadPool(uint32 threads)
 {
     this->activeThreads.Resize(threads);
     this->freeThreads.Resize(threads);
+	this->contexts.Resize(threads);
     
     // setup threads as active
     uint32 i;
@@ -37,9 +39,10 @@ ThreadPool::~ThreadPool()
 /**
 */
 void
-ThreadPool::Enqueue(const std::function<void(byte*, byte*, byte*)>& func)
+ThreadPool::Enqueue(const std::function<void(byte*, byte*, byte*)>& func, byte* inputs, byte* outputs, byte* uniforms)
 {
     this->funcs.Append(func);
+	this->contexts.Append(ThreadFuncContext{ inputs, outputs, uniforms });
 }
 
 //------------------------------------------------------------------------------
@@ -48,7 +51,32 @@ ThreadPool::Enqueue(const std::function<void(byte*, byte*, byte*)>& func)
 void
 ThreadPool::Update()
 {
+	uint32 i;
+	while (this->freeThreads.Size() > 0 && this->funcs.Size() > 0)
+	{
+		const Ptr<Thread>& thread = this->freeThreads[0];
+		const ThreadPoolFunc& func = this->funcs[0];
+		const ThreadFuncContext& context = this->contexts[0];
+		thread->Start<ThreadPoolFunc>(func, context.inputs, context.outputs, context.uniforms);
+		this->funcs.RemoveIndex(0);
+		this->freeThreads.RemoveIndex(0);
+		this->contexts.RemoveIndex(0);
+		this->activeThreads.Append(thread);
+	}
 
+	for (i = 0; i < this->activeThreads.Size(); i++)
+	{
+		const Ptr<Thread>& thread = this->activeThreads[i];
+
+		// stop thread and move to other list if done
+		if (!thread->Running())
+		{
+			thread->Stop();
+			this->freeThreads.Append(thread);
+			this->activeThreads.RemoveIndex(i);
+			i--;
+		}
+	}
 }
 
 }}
